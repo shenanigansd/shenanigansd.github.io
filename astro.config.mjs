1563088903d2f1f69a20dc011b88ae8a85be46c2
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-01-07 16:54:00 -0600

    Init Sentry and Tailwind (#12)
    * Init Tailwind
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Init Sentry
    
    Signed-off-by: GitHub <noreply@github.com>
    
    ---------
    
    Signed-off-by: GitHub <noreply@github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,22 @@ import { defineConfig } from 'astro/config';
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 import vercel from "@astrojs/vercel/serverless";
-
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bradley.dog",
-  integrations: [sentry(), spotlightjs(), preact()],
+  integrations: [
+    sentry({
+      dsn: "https://1555c537c0a21037d9d40f137e608124@o1196120.ingest.sentry.io/4506532398432256",
+      sourceMapsUploadOptions: {
+        project: "bradley-dog",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+    spotlightjs(), preact(), tailwind()
+  ],
   output: "server",
   adapter: vercel({
     webAnalytics: {
