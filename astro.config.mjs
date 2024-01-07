678fbd6ee6204f0273bce10791253e9d53c23c4f
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-01-07 22:45:04 +0000

    Init Tailwind
    Signed-off-by: GitHub <noreply@github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 import vercel from "@astrojs/vercel/serverless";
-
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bradley.dog",
-  integrations: [sentry(), spotlightjs(), preact()],
+  integrations: [sentry(), spotlightjs(), preact(), tailwind()],
   output: "server",
   adapter: vercel({
     webAnalytics: {
