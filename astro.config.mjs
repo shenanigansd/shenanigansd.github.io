9800a1c1403a6c08b15e798c12080fb7d7798492
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-01-07 16:44:10 -0600

    Following the tutorial (#11)
    Signed-off-by: GitHub <noreply@github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,21 @@
 import { defineConfig } from 'astro/config';
-
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sentry(), spotlightjs()],
+  site: "https://bradley.dog",
+  integrations: [sentry(), spotlightjs(), preact()],
   output: "server",
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
     speedInsights: {
-      enabled: true,
-  },
-  }),
+      enabled: true
+    }
+  })
 });
