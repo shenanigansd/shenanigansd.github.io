c9d4b668e1cd82082b986e4bc6f736a257ab885e
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-01-07 20:17:12 +0000

    Setup Vercel Web Analytics and Speed Insights
    Signed-off-by: GitHub <noreply@github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,18 @@ import { defineConfig } from 'astro/config';
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sentry(), spotlightjs()]
+  integrations: [sentry(), spotlightjs()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+  },
+  }),
 });
