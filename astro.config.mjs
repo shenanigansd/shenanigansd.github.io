33abdf093211d768116239289774d54250049aa7
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-06-26 02:45:09 +0000

    Add Sentry
    Signed-off-by: GitHub <noreply@github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sentry from "@sentry/astro";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sentry({
+      dsn: "https://1555c537c0a21037d9d40f137e608124@o1196120.ingest.us.sentry.io/4506532398432256",
+      sourceMapsUploadOptions: {
+        project: "bradley-dog",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
+});
