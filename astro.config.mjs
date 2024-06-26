17a7437735fe5a4a6d353351587553b8116624c6
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-06-26 03:11:45 +0000

    Add Spotlight
    ref: https://spotlightjs.com/setup/astro/
    
    Signed-off-by: GitHub <noreply@github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
 import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sentry({
-      dsn: "https://1555c537c0a21037d9d40f137e608124@o1196120.ingest.us.sentry.io/4506532398432256",
-      sourceMapsUploadOptions: {
-        project: "bradley-dog",
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-      },
-    }),
-  ],
+  integrations: [sentry({
+    dsn: "https://1555c537c0a21037d9d40f137e608124@o1196120.ingest.us.sentry.io/4506532398432256",
+    sourceMapsUploadOptions: {
+      project: "bradley-dog",
+      authToken: process.env.SENTRY_AUTH_TOKEN
+    }
+  }), spotlightjs()]
 });
