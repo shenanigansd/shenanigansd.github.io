9ea26980d5592f34e751e2413fb2fd6a22d58d80
Bradley Reynolds <bradley.reynolds@tailstory.dev>
2025-09-18 04:45:14 +0000

    Re-re-re-init actually minimal this time
    Signed-off-by: Bradley Reynolds <bradley.reynolds@tailstory.dev>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,5 @@
-import { defineConfig } from "astro/config";
-import sentry from "@sentry/astro";
-import spotlightjs from "@spotlightjs/astro";
+// @ts-check
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [sentry({
-    dsn: "https://1555c537c0a21037d9d40f137e608124@o1196120.ingest.us.sentry.io/4506532398432256",
-    sourceMapsUploadOptions: {
-      project: "bradley-dog",
-      authToken: process.env.SENTRY_AUTH_TOKEN
-    }
-  }), spotlightjs()]
-});
+export default defineConfig({});
